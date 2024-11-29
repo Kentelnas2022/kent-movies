40114a6f904be9aa66bd9810b545cc76d0098a05
Kent Zorel B. Elnas <141716943+Kentelnas2022@users.noreply.github.com>
2024-11-29 18:28:08 +0800

    Add files via upload

---
+++ src/components/MovieCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MovieCard = ({ movie }) => (
+  <div className="movie-card">
+    <img src={movie.Poster} alt={movie.Title} />
+    <div className="movie-info">
+      <h3>{movie.Title}</h3>
+      <p>{movie.Year}</p>
+    </div>
+  </div>
+);
+
+export default MovieCard;
